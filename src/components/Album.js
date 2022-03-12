6d5ab5bdbeae242911814d7c23d85690859b88ac
Korinna Pinakoulaki <kpinakoulaki@gmail.com>
2022-03-12 16:01:18 +0000

    Fetch photos within each album

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,7 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
 function Album() {
+  const [photos, setPhotos] = useState([]);
+  const [hasError, setHasError] = useState(false);
+  const { albumId } = useParams();
+
+  useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/photos?_limit=300&albumId=${albumId}`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(photos => setPhotos(photos))
+      .catch(error => {
+        setHasError(true);
+        console.error('error', error);
+      });
+  }, [albumId]);
+
   return (
     <div className="Album">
       <main>
