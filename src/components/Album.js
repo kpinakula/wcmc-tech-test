7ff50e02f96867e5740d1505228ce5fecbe7d0d4
Korinna Pinakoulaki <kpinakoulaki@gmail.com>
2022-03-12 17:48:29 +0000

    Refactor Album data fetching to use AppContext

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,37 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
+import { AppContext } from '../App';
+
 function Album() {
-  const [photos, setPhotos] = useState([]);
-  const [hasError, setHasError] = useState(false);
   const { albumId } = useParams();
-
-  useEffect(() => {
-    const url = `https://jsonplaceholder.typicode.com/photos?_limit=300&albumId=${albumId}`;
-
-    fetch(url)
-      .then(response => response.json())
-      .then(photos => setPhotos(photos))
-      .catch(error => {
-        setHasError(true);
-        console.error('error', error);
-      });
-  }, [albumId]);
+  const { photos, hasError } = useContext(AppContext);
+  const albumPhotos = photos[albumId - 1];
 
   return (
     <div className="Album">
       <main>
         <h1>Album {albumId}</h1>
-        {!hasError && photos.length && (
+        {!hasError && albumPhotos.length && (
           <div className="Tiles">
-            {photos.map(photo => (
-              <Link className="Tile" to={`photo/${photo.id}`} key={photo.id}>
-                <h2>{photo.title}</h2>
-                <img
-                  className="Thumbnail"
-                  src={photo.thumbnailUrl}
-                  alt={photo.title}
-                />
+            {albumPhotos.map(({ id, title, thumbnailUrl }) => (
+              <Link className="Tile" to={`photo/${id}`} key={id}>
+                <h2>{title}</h2>
+                <img className="Thumbnail" src={thumbnailUrl} alt={title} />
               </Link>
             ))}
           </div>
