56d5915317d94dc3575e3084d88e96b09f08e2d4
Korinna Pinakoulaki <kpinakoulaki@gmail.com>
2022-03-12 12:47:11 +0000

    Fetch photos and structure by album

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 function Gallery() {
+  const [albums, setAlbums] = useState([]);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+
+  const fetchPhotos = async () => {
+    const url = 'https://jsonplaceholder.typicode.com/photos?_limit=300';
+
+    fetch(url)
+      .then(response => response.json())
+      .then(photos => {
+        const uniqueAlbumIds = [...new Set(photos.map(photo => photo.albumId))];
+        let photosByAlbum = [];
+        uniqueAlbumIds.forEach(albumId => {
+          photosByAlbum.push(photos.filter(photo => photo.albumId === albumId));
+        });
+        setAlbums(photosByAlbum);
+      })
+      .catch(error => {
+        setHasError(true);
+        console.error('error', error);
+      });
+  };
+
   return (
     <div className="Gallery">
       <main>
         Gallery
-        <Link to="/album/1">Album 1</Link>
-        <Link to="/album/2">Album 2</Link>
+        {!hasError && photos.length && (
+          <div>
+            <Link to="/album/1">Album 1</Link>
+            <Link to="/album/2">Album 2</Link>
+          </div>
+        )}
       </main>
     </div>
   );
