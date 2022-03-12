73e75105c0c9adf4d9a9af5f5f7f8120eb5c2994
Korinna Pinakoulaki <kpinakoulaki@gmail.com>
2022-03-12 18:11:19 +0000

    Add image to photo page component

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { AppContext } from '../App';
 
 function Photo() {
+  const { albumId, photoId } = useParams();
+  const { photos, hasError } = useContext(AppContext);
+  const photo = photos[albumId - 1]?.find(
+    photo => photo.id === Number(photoId)
+  );
+
   return (
     <div className="Photo">
       <main>
-        <p>Photo</p>
+        {!hasError && photo && <img src={photo.url} alt={photo.title} />}
       </main>
     </div>
   );
