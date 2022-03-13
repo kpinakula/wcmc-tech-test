813ce7a8665748ce817ba230a15c199f22f005cc
Korinna Pinakoulaki <kpinakoulaki@gmail.com>
2022-03-13 17:58:19 +0000

    Set photo width responsively

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { AppContext } from '../App';
+import styles from './Photo.module.css';
 
 function Photo() {
   const { albumId, photoId } = useParams();
@@ -11,9 +12,14 @@ function Photo() {
   );
 
   return (
-    <div className="Photo">
+    <div>
       <main>
-        {!hasError && photo && <img src={photo.url} alt={photo.title} />}
+        {!hasError && photo ? (
+          <div>
+            <h1>{photo.title}</h1>
+            <img className={styles.Photo} src={photo.url} alt={photo.title} />
+          </div>
+        ) : null}
       </main>
     </div>
   );
